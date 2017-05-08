264d7161662910aa6621ff5be239bd003da1ac8d
Daniel Randall <dgrandall@me.com>
2017-05-08 11:32:35 -0600

    writing event to elk

--- elkreporting/elkreporting.go
+++ elkreporting/elkreporting.go
@@ -1,6 +1,12 @@
 package elkreporting
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/byuoitav/event-router-microservice/eventinfrastructure"
 	"github.com/byuoitav/event-translator-microservice/common"
 )
@@ -8,6 +14,20 @@ import (
 type elkReporter struct{}
 
 func (e *elkReporter) Write(event eventinfrastructure.Event) {
+	log.Printf("Sending event to : %v", os.Getenv("ELASTIC_API_EVENTS"))
+
+	b, err := json.Marshal(event)
+	if err != nil {
+		log.Printf("error: %v", err.Error())
+	}
+
+	_, err = http.Post(os.Getenv("ELASTIC_API_EVENTS"),
+		"application/json",
+		bytes.NewBuffer(b))
+
+	if err != nil {
+		log.Printf("error: %v", err.Error())
+	}
 }
 
 func (s *elkReporter) SetOutChan(chan<- eventinfrastructure.Event) {

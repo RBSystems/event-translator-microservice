40f8c55d60b857a0cf9057454c9de72f3e183c03
Daniel Randall <dgrandall@me.com>
2017-07-27 10:01:24 -0600

    that worked so fast

--- server.go
+++ server.go
@@ -1,27 +1,21 @@
 package main
 
 import (
-	"os"
-
-	"github.com/byuoitav/event-translator-microservice/handlers"
+	"github.com/byuoitav/event-router-microservice/eventinfrastructure"
 	"github.com/byuoitav/event-translator-microservice/translator"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	sub := eventinfrastructure.NewSubscriber([]string{eventinfrastructure.Translator})
 	port := "7002"
-	routerAddr := ""
-	//Get the router address
-	if len(os.Getenv("LOCAL_ENVIRONMENT")) > 0 {
-		routerAddr = "localhost:7000"
-	}
-	go translator.StartTranslator(routerAddr, port)
+	go translator.StartTranslator(port, sub)
 
 	server := echo.New()
 	server.Pre(middleware.RemoveTrailingSlash())
 	server.Use(middleware.CORS())
 
-	server.POST("/subscribe", handlers.Subscribe)
+	server.POST("/subscribe", sub.HandleSubscriptionRequest)
 	server.Start(":6998")
 }
